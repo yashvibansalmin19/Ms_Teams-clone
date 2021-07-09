0abf6c2d742e87dd3cc7e56c4ed71ac5f7191ae8
yashvibansalmin19 <yashvi.bansal.min19@itbhu.ac.in>
2021-07-09 11:57:26 +0530

    Day 26

--- google_oauth.js
+++ google_oauth.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const jwt = require('jsonwebtoken');
 passport.use(new GoogleStrategy({
-    callbackURL: `https://connect-video-chat.herokuapp.com//auth/google/redirect`,
+    callbackURL: `https://connect-video-chat.herokuapp.com/auth/google/redirect`,
     clientID: "5610789853-cgup6npmfrebr5sl8kfs63h6ovd83ti9.apps.googleusercontent.com",
     clientSecret: "0zA3EONrT63mui2ygwBIyNiX"
 },
